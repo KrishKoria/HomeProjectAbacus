adf3d863ba554a0887640375d1ea36a78dc5c1ec
Krish Koria <krishkoria2004@gmail.com>
2026-04-18 21:44:07 +0530

    remove crap

--- python.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-if exist "%LocalAppData%\Python\bin\python.exe" (
-  "%LocalAppData%\Python\bin\python.exe" %*
-) else (
-  py -3 %*
-)
-exit /b %errorlevel%
--- tests/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Bronze pipeline contract tests."""
